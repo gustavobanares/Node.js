545dbeb17f706aa84fa439c17c14e39ae6a9b271
*Gustavo <gustavobbacco@gmail.com>
2024-12-20 16:18:37 -0300

    feat: options in prompt to system

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -1,8 +1,34 @@
 // moldulos externos
-const inquirer = require('inquirer')
-const chalk = require('chalk')
+const inquirer = require("inquirer").default;
+const chalk = require("chalk");
 
 // modulos internos
-const fs = require('fs')
+const fs = require("fs");
 
-console.log('Iniciando o Accounts')
+operation();
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: [
+          "Criar conta",
+          "Consultar saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) =>{
+
+        const action = answer['action']
+
+        console.log(action)
+
+    })
+    .catch((err) => console.log(err));
+}
