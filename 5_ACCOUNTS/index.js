fb60ff227d4dbb8809626189e02a3f1f4c00e7f8
*Gustavo <gustavobbacco@gmail.com>
2024-12-20 16:40:47 -0300

    feat: finishing account creation

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -37,4 +37,44 @@ function operation() {
 function createAccount() {
   console.log(chalk.bgGreen.black("Parabéns por escolher o nosso banco!"));
   console.log(chalk.green("Defina as opcoes da sua conta a seguir"));
+
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Digite um nome para sua conta:",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+      console.info(accountName);
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Esta conta ja existe escolha outro nome!")
+        );
+        buildAccount();
+        return
+      }
+
+      fs.write1FileSync(
+        `accounts/${accountName}.json`,
+        '{"balance":0}',
+        function (err) {
+          console.log(err);
+        }
+      );
+
+      console.log(chalk.green("Parabens a sua conta foi criada!"));
+      operation();
+    })
+    .catch((err) => console.log(err));
 }
