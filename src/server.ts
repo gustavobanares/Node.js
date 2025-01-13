a39ef64a0203b5b1273a4e4188618f60478a1ac0
*Gustavo <gustavobbacco@gmail.com>
2025-01-12 23:59:11 -0300

    feat: sent request with the get method

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,8 @@ const PORT = 3333
 
 const app = express()
 
+app.get('/', (request, response) =>{
+    response.send('Hello World Express!')
+})
+
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
