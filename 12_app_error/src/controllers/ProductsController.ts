e60126886e4831bafd2f0e739ff3f0034b2662c8
*Gustavo <gustavobbacco@gmail.com>
2025-01-13 01:37:02 -0300

    feat: validations with conditions

--- 12_app_error/src/controllers/ProductsController.ts
+++ 12_app_error/src/controllers/ProductsController.ts
@@ -19,8 +19,20 @@ class ProductsController {
   create(request: Request, response: Response) {
     const { name, price } = request.body;
 
-    if(!name || !price){
-      throw new AppError('Nome e preco do produto sao obrigatorios!')
+    if (!name) {
+      throw new AppError("Nome do produto e obrigatorio!");
+    }
+
+    if(name.trim.length < 6){
+      throw new AppError("Nome do produto precisa ter pelo menos 6 caracteres!");
+    }
+
+    if (!price) {
+      throw new AppError("Preco do produto e obrigatorio!");
+    }
+
+    if (price < 0) {
+      throw new AppError("Preco do produto nao pode ser menor do que zero!");
     }
 
     // throw new Error('Erro ao tentar ao criar um produto!')
