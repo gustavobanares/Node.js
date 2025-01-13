befafdce143a5ab7c06099b3f861b527bdf64dfc
*Gustavo <gustavobbacco@gmail.com>
2025-01-13 00:56:04 -0300

    feat: separating the routes

---
+++ 9_separando_as_rotas/src/routes/product-routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { myMiddleware } from "../middleware/my-middleware";
+
+const productRoutes = Router()
+
+productRoutes.get("/:id", (request, response) => {
+    const { page, limit } = request.query;
+  
+    response.send(`Pagina ${page} de ${limit}`);
+  });
+  
+  productRoutes.post("/", myMiddleware, (request, response) => {
+    const { name, price } = request.body;
+  
+    // response.send(`Produto ${name} custa $${price}`);
+    response.status(201).json({ name, price, user_id:request.user_id });
+  });
+
+export {productRoutes}
