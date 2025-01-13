5d48107a9d3b9f1581d9fd58b76582e850554dae
*Gustavo <gustavobbacco@gmail.com>
2025-01-13 00:17:08 -0300

    feat: data in the request body

---
+++ 5_dados_corpo_requisicao/src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+const PORT = 3333;
+
+const app = express();
+app.use(express.json())
+
+app.get("/products", (request, response) => {
+const {page, limit} = request.query
+
+  response.send(`Pagina ${page} de ${limit}`);
+});
+
+app.post("/products", (request, response) => {
+  const {name, price} = request.body
+  
+    response.send(`Produto ${name} custa $${price}`);
+  });
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
