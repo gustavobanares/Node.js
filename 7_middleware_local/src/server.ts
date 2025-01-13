07cfb697e71d624f45a71c65d3c5e73fa4024dac
*Gustavo <gustavobbacco@gmail.com>
2025-01-13 00:33:43 -0300

    feat: middleware local

---
+++ 7_middleware_local/src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { myMiddleware } from "./middleware/my-middleware";
+
+const PORT = 3333;
+
+const app = express();
+app.use(express.json());
+
+app.use(myMiddleware);
+
+app.get("/products", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Pagina ${page} de ${limit}`);
+});
+
+app.post("/products", myMiddleware, (request, response) => {
+  const { name, price } = request.body;
+
+  // response.send(`Produto ${name} custa $${price}`);
+  response.status(201).json({ name, price });
+});
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
